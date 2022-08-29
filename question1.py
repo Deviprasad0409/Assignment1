8df54b3414b91957f8f174c2f3537da57de2c5a2
Deviprasad K P <111556892+Deviprasad0409@users.noreply.github.com>
2022-08-29 18:14:33 -0500

    Version 1

--- question1.py
+++ question1.py
@@ -1,2 +1,32 @@
+# Code related to Question 1
+from statistics import median, mean
+
 ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-print(ages)
+
+#Using sort() function to sort the list
+ages.sort()
+
+#Displaying Maximum and Minimus age
+print("Minimum age is ",ages[0])
+print("Maximum age is ",ages[-1])
+
+ages.append(ages[0])
+ages.append(ages[-2])
+ages.sort()
+
+#Calculating median
+if(len(ages)%2==0):    #checking if the total number of elements in the list is odd
+
+    median = (ages[int (len(ages)/2)-1] + ages[(int (len(ages)/2))])/2
+
+    print("Median is ", median)
+
+else:
+    median = ages[int (len(ages)/2)]
+
+    print("Median is ", median)
+
+print("Average is ", mean(ages))
+
+print("Range is ", ages[-1]-ages[0])
+
